1a0b81e57b8ebb1686e4f2a086c61422e7356778
vkargin <vkargin@docker2.myguest.virtualbox.org>
2023-12-04 07:48:31 -0500

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
-#!/bin/sh -l
-
 echo "Hello $1"
 time=$(date)
+{%- ifversion actions-save-state-set-output-envs %}
 echo "time=$time" >> $GITHUB_OUTPUT
+{%- else %}
+echo "::set-output name=time::$time"
+{%- endif %}
