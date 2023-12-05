e8420154bc38bf230aaf6d09de015dd3f9f14d10
vkargin <vkargin@docker2.myguest.virtualbox.org>
2023-12-05 07:49:19 -0500

    changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
+#!/bin/sh -l
+
 echo "Hello $1"
 time=$(date)
-{%- ifversion actions-save-state-set-output-envs %}
 echo "time=$time" >> $GITHUB_OUTPUT
-{%- else %}
-echo "::set-output name=time::$time"
-{%- endif %}
